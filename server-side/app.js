ea7739d3a974788b5d50cc7fd6b185498790c4b0
Rodney <54865844+rmartin-sc@users.noreply.github.com>
2025-01-28 21:55:51 +0000

    Some node code

---
+++ server-side/app.js
@@ -0,0 +1,14 @@
+
+
+function repeatString(numIterations, message) {
+    let i = 0;
+    while ( i < numIterations ) {
+        let x = 0;
+        console.log(message);
+        i += 1;
+    }
+}
+
+console.log(i);
+repeatString(5, 'Hello, World!');
+repeatString(3, 'Goodbye, World!');
